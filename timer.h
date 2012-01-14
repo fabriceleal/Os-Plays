08f3a6fb86061474557d6b24a596f838dac18ac3
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-14 12:38:56 -0800

    timer changed

---
+++ timer.h
@@ -0,0 +1,26 @@
+/**
+ * @file timer.h
+ * @author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
+ *
+ * @section LICENSE
+ *
+ * < ... goes here ... >
+ *
+ * @section DESCRIPTION
+ * 
+ * < ... goes here ... >
+ *
+ */
+
+
+// VAMI stands for Validation Against Multiple Includes :)
+
+#ifndef ___VAMI_TIMER
+#define ___VAMI_TIMER
+
+#include "common.h"
+
+void init_timer(u32int frequency);
+
+
+#endif // ___VAMI_TIMER
