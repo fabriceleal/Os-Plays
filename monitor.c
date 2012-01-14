ec6256d48d328ff32babd51d300991da0a664c78
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-14 13:53:09 -0800

    functions for printing decimals and hexs copied :P

--- monitor.c
+++ monitor.c
@@ -19,8 +19,10 @@ static u16int cursor_y = 0;
 static u16int cursor_x = 0;
 
 #define SZ_SCREEN (80*25)
+
 //static u16int video_memory[SZ_SCREEN] = {0};
-static u16int * video_memory = (u16int*)0xB8000;
+static u16int * video_memory = (u16int*)0xB8000; // Start of VGA buffer
+
 #undef SZ_SCREEN
 
 static void move_cursor()
@@ -157,15 +159,75 @@ void monitor_write(char *c)
 
 void monitor_write_hex(u32int n)
 {
-   // TODO: ...
-	return;
+    s32int tmp;
+
+    monitor_write("0x");
+
+    char noZeroes = 1;
+
+    int i;
+    for (i = 28; i > 0; i -= 4)
+    {
+        tmp = (n >> i) & 0xF;
+        if (tmp == 0 && noZeroes != 0)
+        {
+            continue;
+        }
+    
+        if (tmp >= 0xA)
+        {
+            noZeroes = 0;
+            monitor_put (tmp-0xA+'a' );
+        }
+        else
+        {
+            noZeroes = 0;
+            monitor_put( tmp+'0' );
+        }
+    }
+  
+    tmp = n & 0xF;
+    if (tmp >= 0xA)
+    {
+        monitor_put (tmp-0xA+'a');
+    }
+    else
+    {
+        monitor_put (tmp+'0');
+    }
+
 }
 
 void monitor_write_dec(u32int n)
 {
-   // TODO: ...
-	return;
-} 
+
+    if (n == 0)
+    {
+        monitor_put('0');
+        return;
+    }
+
+    s32int acc = n;
+    char c[32];
+    int i = 0;
+    while (acc > 0)
+    {
+        c[i] = '0' + acc%10;
+        acc /= 10;
+        i++;
+    }
+    c[i] = 0;
+
+    char c2[32];
+    c2[i--] = 0;
+    int j = 0;
+    while(i >= 0)
+    {
+        c2[i--] = c[j++];
+    }
+    monitor_write(c2);
+
+}
 
 void printf(){
 	// Refer to vsprintf at the linux0.1 code
