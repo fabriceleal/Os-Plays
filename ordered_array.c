c8b51edf298976179d56a89d31e0071666232d8a
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-16 05:36:33 -0800

    ordered array

---
+++ ordered_array.c
@@ -0,0 +1,17 @@
+/**
+ * @file ordered_array.c
+ * @author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
+ *
+ * @section LICENSE
+ *
+ * < ... goes here ... >
+ *
+ * @section DESCRIPTION
+ * 
+ * < ... goes here ... >
+ *
+ */
+
+#include "ordered_array.h"
+
+
---
+++ ordered_array.h
@@ -0,0 +1,25 @@
+/**
+ * @file ordered_array.h
+ * @author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
+ *
+ * @section LICENSE
+ *
+ * < ... goes here ... >
+ *
+ * @section DESCRIPTION
+ * 
+ * < ... goes here ... >
+ *
+ */
+
+
+// VAMI stands for Validation Against Multiple Includes :)
+
+#ifndef ___VAMI_ORDERED_ARRAY
+#define ___VAMI_ORDERED_ARRAY
+
+// A header file only declares the functions headers and structs used. 
+// Implementations go into the .c file ;)
+
+
+#endif // ___VAMI_ORDERED_ARRAY
