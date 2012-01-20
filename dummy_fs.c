3bb0095efb40427035d05afd014681eb6ab8306e
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-20 12:44:19 -0800

    fs generator ...

---
+++ dummy_fs.c
@@ -0,0 +1,26 @@
+/**
+ * @file dummy_fs.c
+ * @author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
+ *
+ * @section LICENSE
+ *
+ * < ... goes here ... >
+ *
+ * @section DESCRIPTION
+ * 
+ * < ... goes here ... >
+ *
+ */
+
+#include "dummy_fs.h"
+
+#define GEN_FS_SUCCESS 0
+
+/*
+	Generates a dummy fs using a *real* directory as a *model*
+*/
+int generate_dummy_fs(char* model_root_dir)
+{
+
+	return GEN_FS_SUCCESS ;
+}
