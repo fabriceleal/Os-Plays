e26926eea04cc79f0f7eae8e292955f2ef0568d3
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-22 06:36:27 -0800

    files add should have been added in the previous commit ... code files for multitasking

---
+++ task.h
@@ -0,0 +1,51 @@
+/**
+ * @file task.h
+ * @author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
+ *
+ * @section LICENSE
+ *
+ * < ... goes here ... >
+ *
+ * @section DESCRIPTION
+ * 
+ * < ... goes here ... >
+ *
+ */
+
+
+#ifndef ___VAMI_TASK
+#define ___VAMI_TASK
+
+#include "common.h"
+#include "paging.h"
+
+// Will be set to the initial esp value right after entering the main() function
+u32int initial_esp;
+
+// This structure defines a 'task' - a process.
+typedef struct task
+{
+   int id;                // Process ID.
+   u32int esp, ebp;       // Stack and base pointers.
+   u32int eip;            // Instruction pointer.
+   page_directory_t *page_directory; // Page directory.
+   struct task *next;     // The next task in a linked list.
+} task_t;
+
+// Initialises the tasking system.
+void initialise_tasking();
+
+// Called by the timer hook, this changes the running process.
+void task_switch();
+
+// Forks the current process, spawning a new one with a different
+// memory space.
+int fork();
+
+// Causes the current process' stack to be forcibly moved to a new location.
+void move_stack(void *new_stack_start, u32int size);
+
+// Returns the pid of the current process.
+int getpid();
+
+#endif // ___VAMI_TASK
