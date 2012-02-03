31d7ad8c5750ad886ccb62028899d0aa503ead3d
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-02-03 13:45:54 -0800

    already dreaming with mouse :P ...

---
+++ mouse.c
@@ -0,0 +1,22 @@
+/**
+ * @file mouse.c
+ * @author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
+ *
+ * @section LICENSE
+ *
+ * < ... goes here ... >
+ *
+ * @section DESCRIPTION
+ * 
+ * < ... goes here ... >
+ *
+ */
+
+#include "mouse.h"
+
+
+void init_mouse()
+{
+
+}
+
---
+++ mouse.h
@@ -0,0 +1,21 @@
+/**
+ * @file mouse.h
+ * @author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
+ *
+ * @section LICENSE
+ *
+ * < ... goes here ... >
+ *
+ * @section DESCRIPTION
+ * 
+ * < ... goes here ... >
+ *
+ */
+
+
+#ifndef ___VAMI_MOUSE
+#define ___VAMI_MOUSE
+
+
+
+#endif // ___VAMI_MOUSE
